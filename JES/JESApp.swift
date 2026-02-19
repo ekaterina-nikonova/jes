9aaf7ec44a0780208cc282cd50f23c2db86c87de
Ekaterina Nikonova <ekaterina@sixty-north.com>
2026-02-19 18:08:14 +0100

    Initial Commit

---
+++ JES/JESApp.swift
@@ -0,0 +1,17 @@
+//
+//  JESApp.swift
+//  JES
+//
+//  Created by Ekaterina Nikonova on 19/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct JESApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
